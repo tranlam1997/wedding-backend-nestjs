e9d25763d9f31b6d6d81d070f0d9c9a2c1f6f6f2
Tran Tuan Lam <foet1997@gmail.com>
2023-02-09 00:59:16 +0700

    Add district model

---
+++ src/modules/address/interfaces/district.interface.ts
@@ -0,0 +1,13 @@
+export interface IDistrict {
+    id: number;
+    code: string;
+    name: string;
+    nameEn: string;
+    fullName: string;
+    fullNameEn: string;
+    codeName: string;
+    province: any;
+    provinceCode: string;
+    administrativeUnit: any;
+    administrativeUnitId: number;
+}
---
+++ src/modules/address/models/district.model.ts
@@ -0,0 +1,57 @@
+import { Column, DataType, Model, PrimaryKey, Table, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import { IDistrict } from '../interfaces/district.interface';
+import { AdministrativeUnit } from "./administrative-unit.model";
+import { Province } from "./province.model";
+
+@Table({
+  underscored: true,
+  timestamps: true,
+  freezeTableName: true,
+})
+export class District extends Model<IDistrict> {
+  @PrimaryKey
+  @Column(DataType.CHAR(20))
+  code: string;
+
+  @Column(DataType.CHAR(255))
+  name: string;
+
+  
+  @Column({
+    type: DataType.CHAR(255),
+    allowNull: true,
+  })
+  nameEn: string;
+
+  @Column({
+    type: DataType.CHAR(255),
+    allowNull: true,
+  })
+  fullName: string;
+
+  @Column({
+    type: DataType.CHAR(255),
+    allowNull: true,
+  })
+  fullNameEn: string;
+
+  @Column({
+    type: DataType.CHAR(255),
+    allowNull: true,
+  })
+  codeName: string;
+
+  @ForeignKey(() => Province)
+  @Column
+  provinceCode: string;
+
+  @ForeignKey(() => AdministrativeUnit)
+  @Column
+  administrativeUnitId: number;
+
+  @BelongsTo(() => Province)
+  province: Province;
+
+  @BelongsTo(() => AdministrativeUnit)
+  administrativeUnit: AdministrativeUnit;
+}
