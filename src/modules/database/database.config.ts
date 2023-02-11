f85b883672ad158869469ff480cc7a1659ef1682
Tran Tuan Lam <foet1997@gmail.com>
2023-02-11 23:03:13 +0700

    update database module

--- src/modules/database/database.config.ts
+++ src/modules/database/database.config.ts
@@ -5,5 +5,5 @@ export default registerAs('database', () => ({
     port: parseInt(process.env.DB_PORT, 10),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
 }))
--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
 import databaseConfig from './database.config';
 
 @Module({
   imports: [
     SequelizeModule.forRootAsync({
       imports: [ConfigModule.forFeature(databaseConfig)],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService) =>  {        
+      return {
         dialect: 'mysql',
         host: configService.get('database.host'),
         port: configService.get('database.port'),
         username: configService.get('database.username'),
         password: configService.get('database.password'),
         database: configService.get('database.database'),
-      }),
+        autoLoadModels: true,
+        synchronize: true,
+        logging: false
+      } as SequelizeModuleOptions},
       inject: [ConfigService],
     }),
   ],
