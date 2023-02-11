c5fc219492d5aff277700d949b31bf52122a7ac7
Tran Tuan Lam <foet1997@gmail.com>
2023-02-11 23:02:22 +0700

    add client model and interface

---
+++ src/modules/client/interfaces/client.interface.ts
@@ -0,0 +1,5 @@
+export interface IClient {
+    version: string;
+    type: string;
+    value: string;
+}
---
+++ src/modules/client/models/client.model.ts
@@ -0,0 +1,27 @@
+import { Column, Model, Table, DataType } from 'sequelize-typescript';
+import { IClient } from '../interfaces/client.interface';
+
+@Table({
+    underscored: true,
+    timestamps: true,
+    freezeTableName: false,
+})
+export class Client extends Model<IClient> {
+    @Column({
+        type: DataType.CHAR(21),
+        allowNull: true,
+    })
+    version: string;
+
+    @Column({
+        type: DataType.CHAR(20),
+        allowNull: true,
+    })
+    type: string;
+
+    @Column({
+        type: DataType.CHAR(50),
+        allowNull: true,
+    })
+    value: string;
+}
