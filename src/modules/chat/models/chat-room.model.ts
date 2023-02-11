ec58d044e733103bc322d6e534206ba7bbe6a34e
Tran Tuan Lam <foet1997@gmail.com>
2023-02-11 23:01:35 +0700

    add chat models

---
+++ src/modules/chat/models/chat-room.model.ts
@@ -0,0 +1,37 @@
+import { Column, Model, Table, DataType, ForeignKey, HasMany } from 'sequelize-typescript';
+import { IChatRoom } from '../interfaces/chat-room.interface';
+import { User } from '../../user/models/user.model';
+
+
+@Table({
+    underscored: true,
+    timestamps: true,
+    freezeTableName: false,
+  })
+export class ChatRoom extends Model<IChatRoom> {
+    @Column(DataType.CHAR(100))
+    name: string;
+
+    @Column(DataType.CHAR(100))
+    photoId: string;
+
+    @Column(DataType.CHAR(100))
+    backgroundId: string;
+
+    @Column(DataType.CHAR(100))
+    type: string;
+
+    @Column(DataType.CHAR(100))
+    description: string;
+
+    @Column(DataType.CHAR(100))
+    extra: string;
+
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.INTEGER,
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE'
+    })
+    userId: number;
+}
---
+++ src/modules/chat/models/participant.model.ts
@@ -0,0 +1,34 @@
+import { Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript';
+import { IParticipant } from '../interfaces/participant.interface';
+import { User } from '../../user/models/user.model';
+import { ChatRoom } from './chat-room.model';
+
+@Table({
+    underscored: true,
+    timestamps: true,
+    freezeTableName: false,
+})
+export class Participant extends Model<IParticipant> {
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.INTEGER,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        unique: 'userId_roomId'
+    })
+    userId: number;
+
+    @ForeignKey(() => ChatRoom)
+    @Column({
+        type: DataType.INTEGER,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        unique: 'userId_roomId'
+    })
+    roomId: number;
+
+    @Column({
+        type: DataType.BIGINT,
+    })
+    time: number;
+}
