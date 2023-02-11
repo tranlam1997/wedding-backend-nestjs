229e62655ea31a42fd317bede5f77c5dd8b2db19
Tran Tuan Lam <foet1997@gmail.com>
2023-02-11 23:15:37 +0700

    update app module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,16 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AddressModule } from '../address/address.module';
 import { AuthModule } from '../auth/auth.module';
-import { ConfigModuleCustom } from '../config/config.module';
-import { CustomersModule } from '../customers/customers.module';
+import { CustomerModule } from '../customer/customer.module';
 import { DatabaseModule } from '../database/database.module';
-import { EventsModule } from '../events/events.module';
-import { UsersModule } from '../users/users.module';
+import { EventModule } from '../event/event.module';
+import { UserModule } from '../user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from '../chat/chat.module';
+import { ClientModule } from '../client/client.module';
+import { MenuModule } from '../menu/menu.module';
+import { MessageModule } from '../message/message.module';
+import { RoleModule } from '../role/role.module';
+import { TransactionModule } from '../transaction/transaction.module';
+import { ConfigModule } from '@nestjs/config';
+import { AddressModule } from '../address/address.module';
+import { AttachmentModule } from '../attachment/attachment.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModuleCustom, CustomersModule, AddressModule, AuthModule, EventsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule, 
+    CustomerModule, 
+    AttachmentModule,
+    AddressModule, 
+    AuthModule, 
+    EventModule, 
+    UserModule,
+    ChatModule,
+    ClientModule,
+    MenuModule,
+    MessageModule,
+    RoleModule,
+    TransactionModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
