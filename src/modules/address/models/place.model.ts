9aa1d554291502370ffa05db09945cd1fa6252c0
Tran Tuan Lam <foet1997@gmail.com>
2023-02-11 23:15:11 +0700

    update place table name

--- src/modules/address/models/place.model.ts
+++ src/modules/address/models/place.model.ts
@@ -2,11 +2,11 @@ import { Column, DataType, Model, Table } from "sequelize-typescript";
 import { IPlace } from "../interfaces/place.interface";
 
 @Table({
-    underscored: true,
-    timestamps: true,
-    freezeTableName: true,
-  })
+  underscored: true,
+  timestamps: true,
+  freezeTableName: false,
+})
 export class Place extends Model<IPlace> {
-    @Column(DataType.CHAR(50))
-    name: string;
+  @Column(DataType.CHAR(50))
+  name: string;
 }
