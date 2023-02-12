9e595042df2a459572f1a30cf32b506e268c8b02
Tran Tuan Lam <foet1997@gmail.com>
2023-02-13 00:02:32 +0700

    update ping service

--- src/modules/ping/ping.service.ts
+++ src/modules/ping/ping.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import {db} from '../../common/firebase/firebase-config'
 @Injectable()
 export class PingService {
   constructor(
   ) {}
 
   async pingToUser(userId: number) {
-
+    const ref = db.ref(`/ping/${userId}`);
+    return ref.once('value', data => {
+        return data
+    })
   }
 }
