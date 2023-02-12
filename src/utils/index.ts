56804e877d302d6c9cd61009c362a5b98a8aabdd
Tran Tuan Lam <foet1997@gmail.com>
2023-02-12 23:59:23 +0700

    update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,40 +1,44 @@
-const stripDomain = (url: string) => {
+import request from 'request';
+
+export const stripDomain = (url: string) => {
     return url.replace(/^.*\/\/[^\/]+/, '');
 }
 
-const stripAccents = (str: string) => {
+export const stripAccents = (str: string) => {
     if (!str) return str;
     return str.normalize('NFD')
         .replace(/[\u0300-\u036f]/g, '')
         .replace(/đ/g, 'd').replace(/Đ/g, 'D');
 }
 
-const extractAttachmentId = (s: string) => {
+export const extractAttachmentId = (s: string) => {
     if (!s) return ''
     const index = s.lastIndexOf('/')
     return s.substring(index + 1, s.length)
 }
 
-const removeAllDolarAndComma = (s: string) => {
+export const removeAllDolarAndComma = (s: string) => {
     if(!s) return ''
     return s.replace(/[\$\,]/g, '_');
 }
 
-const encodeAttachment = (attachment: { id: any; fileName: any; size: any; type: any; width: any; height: any; }) => {
+export const encodeAttachment = (attachment: { id: any; fileName: any; size: any; type: any; width: any; height: any; }) => {
     const { id, fileName, size, type, width, height } = attachment;
     return `${id}$${fileName}$${type}$${size}${width}${height}`
 }
 
-const encodeAttachments = (items: any[]) => {
+export const encodeAttachments = (items: any[]) => {
     return items.map((e: any) => encodeAttachment(e)).join(',')
 }
 
-
-module.exports = {
-    stripDomain,
-    stripAccents,
-    extractAttachmentId,
-    removeAllDolarAndComma,
-    encodeAttachment,
-    encodeAttachments
+export function doRequest(url: string) {
+	return new Promise(function (resolve, reject) {
+		request(url, function (error, res, body) {
+			if (!error && res.statusCode === 200) {
+				resolve(body);
+			} else {
+				reject(error);
+			}
+		});
+	});
 }
--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -2,19 +2,19 @@
 import fs from 'fs'
 import S3 from 'aws-sdk/clients/s3'
 
-const bucketName = "springsocialstorage"
+export const bucketName = "springsocialstorage"
 const region = "ap-southeast-1"
 const accessKeyId = "AKIAVA2D7TWXKTZIUH5T"
 const secretAccessKey = "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"
 
-const s3 = new S3({
+export const s3 = new S3({
   region,
   accessKeyId,
   secretAccessKey
 })
 
 // uploads a file to s3
-function uploadFile(file: { path: fs.PathLike; filename: any }) {
+export function uploadFile(file: { path: fs.PathLike; filename: any }) {
   const fileStream = fs.createReadStream(file.path)
 
   const uploadParams = {
@@ -27,7 +27,7 @@ function uploadFile(file: { path: fs.PathLike; filename: any }) {
 }
 
 // uploads a file to s3
-function uploadBuffer(buffer: any, key: any) {
+export function uploadBuffer(buffer: any, key: any) {
   const uploadParams = {
     Bucket: bucketName,
     Body: buffer,
@@ -37,7 +37,7 @@ function uploadBuffer(buffer: any, key: any) {
   return s3.upload(uploadParams).promise()
 }
 // uploads a file to s3
-function uploadBufferImageBase64(buffer: any, key: any) {
+export function uploadBufferImageBase64(buffer: any, key: any) {
   const uploadParams = {
     Bucket: bucketName,
     Body: buffer,
@@ -48,13 +48,9 @@ function uploadBufferImageBase64(buffer: any, key: any) {
 
   return s3.upload(uploadParams).promise()
 }
-exports.uploadFile = uploadFile
-exports.uploadBuffer = uploadBuffer
-exports.uploadBufferImageBase64 = uploadBufferImageBase64
-
 
 // downloads a file from s3
-function getFileStream(fileKey: any) {
+export function getFileStream(fileKey: any) {
   const downloadParams = {
     Key: fileKey,
     Bucket: bucketName
@@ -62,7 +58,3 @@ function getFileStream(fileKey: any) {
 
   return s3.getObject(downloadParams).createReadStream()
 }
-exports.getFileStream = getFileStream
-
-exports.s3 = s3;
-exports.bucketName = bucketName;
